c32b2b9611124775d91e8ca11e3db7fadfb1645d
AyonDebnath <ayonnath.31@gmail.com>
2024-04-20 13:37:29 -0230

    Lab: Mangaing state in React

--- customizing-example/src/App.js
+++ customizing-example/src/App.js
@@ -1,15 +1,18 @@
-function App() {
-    function handleClick() {
-        let randomNum = Math.floor(Math.random() * 3) + 1;
-        console.log(randomNum);
-        let userInput = prompt('type a number');
-        alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-    }
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
+import React from "react";
 
+function App() {
+    const [fruits] = React.useState([
+        {fruitName: 'apple', id: 1},
+        {fruitName: 'apple', id: 2},
+        {fruitName: 'plum', id: 3},
+    ]);
     return (
-        <div>
-            <h1>Task: Add a button and handle a click event</h1>
-            <button onClick={handleClick}>Guess the number between 1 and 3.</button>
+        <div className="App">
+            <h1>Where should the state go?</h1>
+            <Fruits fruits = {fruits}/>
+            <FruitsCounter fruits = {fruits}/>
         </div>
     );
 }
